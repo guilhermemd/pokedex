67154ffcc29e36e7bd7b7cbe43823744b8b6baff
guilhermemd <guilherme.md@gmail.com>
2021-05-01 21:29:43 -0300

    pokedex

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+
+
+  render(){
+    return (
+     <div className="App">
+        <h1> Pokedex </h1>
+          <Pokedex />
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+        
+        this.typesOfPokemon = ['All', ...new Set(pokemons.map((pokemon) => pokemon.type))].sort();
+        this.state = {
+          pokemons: pokemons,
+          pokemonLength: pokemons.length,
+          currentIndex: 0
+        }
+        this.nextPokemon = this.nextPokemon.bind(this)
+        console.log(this.typesOfPokemon)
+
+      }
+
+      nextPokemon() {
+        this.setState((previuosState, _props) => {
+          const { currentIndex, pokemonLength } = previuosState;
+          const indexAtual = currentIndex === pokemonLength - 1 ? 0 : currentIndex + 1;
+          return { ...previuosState, currentIndex: indexAtual }
+        })
+    
+      }
     render() {
-        return (
+         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={this.state.pokemons[this.state.currentIndex].id} pokemon={this.state.pokemons[this.state.currentIndex]}/>
+                <button onClick={this.nextPokemon}> Next Pokemon</button>
             </div>
         );
     }
